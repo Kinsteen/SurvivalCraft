82fafba9a098313612c8a904bcae48721b5c8a86
pitiqui <pitiqui@hotmail.fr>
2014-09-06 15:37:50 +0200

    Fixs some bugs and add fireworks. Fireworks. May be finish ?

--- SurvivalCraft/src/fr/pitiqui/survivalcraft/GameManager.java
+++ SurvivalCraft/src/fr/pitiqui/survivalcraft/GameManager.java
@@ -1,16 +1,22 @@
 package fr.pitiqui.survivalcraft;
 
 import java.io.File;
+import java.util.Random;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -18,6 +24,7 @@ import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.Plugin;
 
 public class GameManager implements Listener
@@ -106,30 +113,86 @@ public class GameManager implements Listener
 	@EventHandler
 	public void onPlayerDead(PlayerDeathEvent e)
 	{
-		e.getEntity().kickPlayer("Vous avez perdu ! Bonne chance pour la prochaine partie !");
+		ScoreboardManager.numPlayers--;
+		
 		for(Player p : Bukkit.getServer().getOnlinePlayers())
 		{
 			p.playSound(p.getLocation(), Sound.WITHER_SPAWN, 10, 1);
 		}
 		
-		if(Bukkit.getServer().getOnlinePlayers().length == 1)
+		if(ScoreboardManager.numPlayers == 1)
 		{
-			for(final Player p : Bukkit.getServer().getOnlinePlayers())
+			Player winner = null;
+			
+			for(Player temp : Bukkit.getServer().getOnlinePlayers())
 			{
-				p.sendMessage(ChatColor.GREEN + "Bravo ! Vous avez gagné ! Bonne chance pour la prochaine partie !");
+				if(temp != e.getEntity())
+				{
+					winner = temp;
+				}
+			}
+				winner.sendMessage(ChatColor.GREEN + "Bravo ! Vous avez gagné ! Bonne chance pour la prochaine partie !");
+				Firework fw = Bukkit.getWorld("sg").spawn(winner.getLocation(), Firework.class);
+				FireworkMeta fwm = fw.getFireworkMeta();
+	            Random r = new Random();   
+	            
+	            //Get the type
+	            int rt = r.nextInt(4) + 1;
+	            Type type = Type.BALL;       
+	            if (rt == 1) type = Type.BALL;
+	            if (rt == 2) type = Type.BALL_LARGE;
+	            if (rt == 3) type = Type.BURST;
+	            if (rt == 4) type = Type.CREEPER;
+	            if (rt == 5) type = Type.STAR;
+	           
+	            //Get our random colours   
+	            int r1i = r.nextInt(17) + 1;
+	            int r2i = r.nextInt(17) + 1;
+	            Color c1 = getColor(r1i);
+	            Color c2 = getColor(r2i);
+	           
+	            //Create our effect with this
+	            FireworkEffect effect = FireworkEffect.builder().flicker(r.nextBoolean()).withColor(c1).withFade(c2).with(type).trail(r.nextBoolean()).build();
+	           
+	            //Then apply the effect to the meta
+	            fwm.addEffect(effect);
+	           
+	            //Generate some random power and set it
+	            int rp = r.nextInt(2) + 1;
+	            fwm.setPower(rp);
+	           
+	            //Then apply this to our rocket
+	            fw.setFireworkMeta(fwm);
+	            
+	            final Player winnertemp = winner;
 				Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getPlugin(Main.class), new Runnable() {
 					
 					@Override
 					public void run() {
-						p.kickPlayer("Bravo !");
+						winnertemp.kickPlayer("Bravo !");
+						endGame();
 					}
 				}, 200L);
-			}
 		}
 	}
 	
 	@EventHandler
+	public void onPlayerRespawn(PlayerRespawnEvent e)
+	{
+		e.getPlayer().kickPlayer("Vous avez perdu ! Bonne chance pour la prochaine partie !");
+	}
+	
+	@EventHandler
 	public void onBlockBreakEvent(BlockBreakEvent e)
+	{
+		if(Main.joinable != "ingame")
+		{
+			e.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void onBlockPlaceEvent(BlockPlaceEvent e)
 	{
 		if(Main.joinable != "ingame")
 		{
@@ -161,4 +224,61 @@ public class GameManager implements Listener
 			ev.setCancelled(true);
 		}
 	}
+	
+	static Color getColor(int i) {
+		Color c = null;
+		if(i==1){
+		c=Color.AQUA;
+		}
+		if(i==2){
+		c=Color.BLACK;
+		}
+		if(i==3){
+		c=Color.BLUE;
+		}
+		if(i==4){
+		c=Color.FUCHSIA;
+		}
+		if(i==5){
+		c=Color.GRAY;
+		}
+		if(i==6){
+		c=Color.GREEN;
+		}
+		if(i==7){
+		c=Color.LIME;
+		}
+		if(i==8){
+		c=Color.MAROON;
+		}
+		if(i==9){
+		c=Color.NAVY;
+		}
+		if(i==10){
+		c=Color.OLIVE;
+		}
+		if(i==11){
+		c=Color.ORANGE;
+		}
+		if(i==12){
+		c=Color.PURPLE;
+		}
+		if(i==13){
+		c=Color.RED;
+		}
+		if(i==14){
+		c=Color.SILVER;
+		}
+		if(i==15){
+		c=Color.TEAL;
+		}
+		if(i==16){
+		c=Color.WHITE;
+		}
+		if(i==17){
+		c=Color.YELLOW;
+		}
+		 
+		return c;
+		}
 }
--- SurvivalCraft/src/fr/pitiqui/survivalcraft/PreEventManager.java
+++ SurvivalCraft/src/fr/pitiqui/survivalcraft/PreEventManager.java
@@ -1,8 +1,16 @@
 package fr.pitiqui.survivalcraft;
 
+import java.util.Random;
+
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.FireworkEffect.Type;
+import org.bukkit.entity.Firework;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -10,6 +18,7 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkMeta;
 
 public class PreEventManager implements Listener
 {
@@ -60,6 +69,62 @@ public class PreEventManager implements Listener
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent e)
 	{
+		ScoreboardManager.numPlayers--;
+		
+		if(ScoreboardManager.numPlayers == 1)
+		{
+			Player winner = null;
+			
+			for(Player temp : Bukkit.getServer().getOnlinePlayers())
+			{
+				if(temp != e.getPlayer())
+				{
+					winner = temp;
+				}
+			}
+				winner.sendMessage(ChatColor.GREEN + "Bravo ! Vous avez gagné ! Bonne chance pour la prochaine partie !");
+				Firework fw = Bukkit.getWorld("sg").spawn(winner.getLocation(), Firework.class);
+				FireworkMeta fwm = fw.getFireworkMeta();
+	            Random r = new Random();   
+	            
+	            //Get the type
+	            int rt = r.nextInt(4) + 1;
+	            Type type = Type.BALL;       
+	            if (rt == 1) type = Type.BALL;
+	            if (rt == 2) type = Type.BALL_LARGE;
+	            if (rt == 3) type = Type.BURST;
+	            if (rt == 4) type = Type.CREEPER;
+	            if (rt == 5) type = Type.STAR;
+	           
+	            //Get our random colours   
+	            int r1i = r.nextInt(17) + 1;
+	            int r2i = r.nextInt(17) + 1;
+	            Color c1 = GameManager.getColor(r1i);
+	            Color c2 = GameManager.getColor(r2i);
+	           
+	            //Create our effect with this
+	            FireworkEffect effect = FireworkEffect.builder().flicker(r.nextBoolean()).withColor(c1).withFade(c2).with(type).trail(r.nextBoolean()).build();
+	           
+	            //Then apply the effect to the meta
+	            fwm.addEffect(effect);
+	           
+	            //Generate some random power and set it
+	            int rp = r.nextInt(2) + 1;
+	            fwm.setPower(rp);
+	           
+	            //Then apply this to our rocket
+	            fw.setFireworkMeta(fwm);
+	            
+	            final Player winnertemp = winner;
+				Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getPlugin(Main.class), new Runnable() {
+					
+					@Override
+					public void run() {
+						winnertemp.kickPlayer("Bravo !");
+						GameManager.endGame();
+					}
+				}, 200L);
+		}
 	}
 	
 	@EventHandler
